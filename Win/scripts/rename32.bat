9747ff1b3e496d3de918d36b66ffe3993086f66d
frederik.vernelen <frederik.vernelen@f586500c-2d08-b1d5-d43c-470b212bf771>
2011-03-30 12:11:56 +0000

    bugfix : scripts path

--- Win/scripts/rename32.bat
+++ Win/scripts/rename32.bat
@@ -1,10 +1,10 @@
 @echo on
 
-svn info | FindStr /L Revision > %~dp0/svn_info.txt
+svn info | FindStr /L Revision > %~dp0\svn_info.txt
 
-for /f "tokens=2" %%T in (%~dp0/svn_info.txt) do set SVNRevision=%%T
+for /f "tokens=2" %%T in (%~dp0svn_info.txt) do set SVNRevision=%%T
 
-cp %~dp0/../Release/EDTWin32.exe %~dp0/../builds/EDTWin32_%SVNRevision%.exe
+cp %~dp0..\Release\EDTWin32.exe %~dp0\..\builds\EDTWin32_%SVNRevision%.exe
 
 
 @echo off
