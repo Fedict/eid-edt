d8e879bd5c14558bfe488102a3025fc498ccaf3f
frederik.vernelen@gmail.com <frederik.vernelen@gmail.com@f586500c-2d08-b1d5-d43c-470b212bf771>
2011-04-05 14:52:56 +0000

    changed the rename bat files, so no svn.exe dependency exists anymore

--- Win/scripts/rename32.bat
+++ Win/scripts/rename32.bat
@@ -1,10 +1,8 @@
+@echo off
+
+For /F "tokens=1-2 delims=[]" %%T in ('Find /N /V "" %~dp0..\.svn\entries') Do (
+If "%%T"=="4" set SVNRevision=%%T)
 @echo on
-
-svn info | FindStr /L Revision > %~dp0\svn_info.txt
-
-for /f "tokens=2" %%T in (%~dp0svn_info.txt) do set SVNRevision=%%T
-
-cp %~dp0..\Release\EDTWin32.exe %~dp0\..\builds\EDTWin32_%SVNRevision%.exe
-
+copy %~dp0..\release\EDTWin32.exe %~dp0..\builds\EDTWin32_%SVNRevision%.exe
 
 @echo off
--- Win/scripts/rename64.bat
+++ Win/scripts/rename64.bat
@@ -1,10 +1,8 @@
+@echo off
+
+For /F "tokens=1-2 delims=[]" %%T in ('Find /N /V "" %~dp0..\.svn\entries') Do (
+If "%%T"=="4" set SVNRevision=%%U)
 @echo on
-
-svn info | FindStr /L Revision > %~dp0/svn_info.txt
-
-for /f "tokens=2" %%T in (%~dp0/svn_info.txt) do set SVNRevision=%%T
-
-cp %~dp0../x64/release/EDTx64.exe %~dp0../builds/EDTWin64_%SVNRevision%.exe
-
+copy %~dp0..\x64\release\EDTx64.exe %~dp0..\builds\EDTx64_%SVNRevision%.exe
 
 @echo off
