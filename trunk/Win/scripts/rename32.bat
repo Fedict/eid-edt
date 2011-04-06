43355e49747cd4e35e6c21daef13d207610ee37f
frederik.vernelen <frederik.vernelen@f586500c-2d08-b1d5-d43c-470b212bf771>
2011-04-06 14:57:40 +0000

    bugfix in rename32.bat

--- trunk/Win/scripts/rename32.bat
+++ trunk/Win/scripts/rename32.bat
@@ -1,7 +1,7 @@
 @echo off
 
 For /F "tokens=1-2 delims=[]" %%T in ('Find /N /V "" %~dp0..\.svn\entries') Do (
-If "%%T"=="4" set SVNRevision=%%T)
+If "%%T"=="4" set SVNRevision=%%U)
 @echo on
 copy %~dp0..\release\EDTWin32.exe %~dp0..\builds\EDTWin32_%SVNRevision%.exe
 
