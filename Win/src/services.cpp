a3b335d260c297ebb3aea5b3d08509c97163b384
frederik.vernelen <frederik.vernelen@f586500c-2d08-b1d5-d43c-470b212bf771>
2011-05-04 13:59:15 +0000

    Added the 'Log On As' data of the services

--- Win/src/services.cpp
+++ Win/src/services.cpp
@@ -26,7 +26,8 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////
 int serviceGetInfoFromManagerHandle(SC_HANDLE schManager, LPCTSTR lpServiceName);
 const wchar_t *serviceGetStatusLabel(int Status);
-int serviceGetInfoFromServiceHandle(SC_HANDLE schService, LPCTSTR lpServiceName);
+int serviceGetStatusInfoFromServiceHandle(SC_HANDLE schService);
+int serviceGetConfigInfoFromServiceHandle(SC_HANDLE schService);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////// PUBLIC FUNCTIONS /////////////////////////////////////////////////
@@ -43,7 +44,7 @@ int EDT_StartServicesLog ()
 	LOG(EDT_LINE_BREAK);
 	LOG_SCREEN(L"logging services' info...\r\n");
 
-	if( NULL == (schManager = OpenSCManager( NULL, SERVICES_ACTIVE_DATABASE,GENERIC_READ )) ) 
+	if( (schManager = OpenSCManager( NULL, SERVICES_ACTIVE_DATABASE,GENERIC_READ )) == NULL ) 
 	{
 		LOG_LASTERROR(L"OpenSCManager failed");
 		iReturnCode = EDT_ERR_INTERNAL;
@@ -57,7 +58,7 @@ int EDT_StartServicesLog ()
 		ENUM_SERVICE_STATUS_PROCESS *pStatus = NULL;
 
 		//First call to get the size of the buffer
-		if(0 != EnumServicesStatusEx( schManager,						// __in        SC_HANDLE hSCManager,
+		if(EnumServicesStatusEx( schManager,						// __in        SC_HANDLE hSCManager,
 							  SC_ENUM_PROCESS_INFO,				// __in        SC_ENUM_TYPE InfoLevel,
 							  SERVICE_DRIVER | SERVICE_WIN32,	// __in        DWORD dwServiceType,
 							  SERVICE_STATE_ALL,				// __in        DWORD dwServiceState,
@@ -67,7 +68,7 @@ int EDT_StartServicesLog ()
 							  &dwNumberOfService,				//__out        LPDWORD lpServicesReturned,
 							  NULL,								//__inout_opt  LPDWORD lpResumeHandle,
 							  NULL								//__in_opt     LPCTSTR pszGroupName
-							  ))
+							  ) != 0)
 		{
 			LOG_LASTERROR(L"EnumServicesStatusEx failed to get size needed");
 			iReturnCode = EDT_ERR_INTERNAL;
@@ -90,7 +91,7 @@ int EDT_StartServicesLog ()
 				LOG_ERROR(L"Memory allocation failed");
 				iReturnCode = EDT_ERR_INTERNAL;
 			}
-			else if(0 == EnumServicesStatusEx( schManager,				// __in        SC_HANDLE hSCManager,
+			else if(EnumServicesStatusEx( schManager,				// __in        SC_HANDLE hSCManager,
 				SC_ENUM_PROCESS_INFO,				// __in        SC_ENUM_TYPE InfoLevel,
 				SERVICE_DRIVER | SERVICE_WIN32,	// __in        DWORD dwServiceType,
 				SERVICE_STATE_ALL,				// __in        DWORD dwServiceState,
@@ -100,7 +101,7 @@ int EDT_StartServicesLog ()
 				&dwNumberOfService,				//__out        LPDWORD lpServicesReturned,
 				NULL,								//__inout_opt  LPDWORD lpResumeHandle,
 				NULL								//__in_opt     LPCTSTR pszGroupName
-				))
+				) == 0)
 			{
 				LOG_LASTERROR(L"EnumServicesStatusEx failed");
 				iReturnCode = EDT_ERR_INTERNAL;
@@ -177,7 +178,7 @@ int serviceGetInfoFromManagerHandle(SC_HANDLE schManager, LPCTSTR lpServiceName)
 
 	SC_HANDLE schService=0;
 
-	if( NULL == (schService = OpenService( schManager, lpServiceName, SERVICE_QUERY_STATUS | SERVICE_QUERY_CONFIG )) ) 
+	if( (schService = OpenService( schManager, lpServiceName, SERVICE_QUERY_STATUS | SERVICE_QUERY_CONFIG )) == NULL ) 
 	{
 		switch( GetLastError() ) 
 		{
@@ -193,7 +194,11 @@ int serviceGetInfoFromManagerHandle(SC_HANDLE schManager, LPCTSTR lpServiceName)
 	}
 	else 
 	{
-		iReturnCode = serviceGetInfoFromServiceHandle(schService,lpServiceName);
+		iReturnCode = serviceGetStatusInfoFromServiceHandle(schService);
+		if(iReturnCode == EDT_OK)
+			iReturnCode = serviceGetConfigInfoFromServiceHandle(schService);
+		else
+			serviceGetConfigInfoFromServiceHandle(schService);
 	}
 
     if(schService) CloseServiceHandle(schService); 
@@ -202,7 +207,7 @@ int serviceGetInfoFromManagerHandle(SC_HANDLE schManager, LPCTSTR lpServiceName)
 } 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
-int serviceGetInfoFromServiceHandle(SC_HANDLE schService,  LPCTSTR lpServiceName)
+int serviceGetStatusInfoFromServiceHandle(SC_HANDLE schService)
 {
 	int iReturnCode = EDT_OK;
 
@@ -218,12 +223,12 @@ int serviceGetInfoFromServiceHandle(SC_HANDLE schService,  LPCTSTR lpServiceName
 	//QueryServiceStatusEx
 	SERVICE_STATUS_PROCESS *pStatus = NULL;
 	//First call to get the size of the buffer
-	if( 0 != QueryServiceStatusEx(schService,				//__in       SC_HANDLE hService,
+	if( QueryServiceStatusEx(schService,				//__in       SC_HANDLE hService,
 							      SC_STATUS_PROCESS_INFO,	//__in       SC_STATUS_TYPE InfoLevel,
 							      NULL,						//__out_opt  LPBYTE lpBuffer,
 							      0,						//__in       DWORD cbBufSize,
 							      &dwBytesNeeded			//__out      LPDWORD pcbBytesNeeded
-							      ))
+							      ) != 0 )
 	{
 		LOG_LASTERROR(L"QueryServiceStatusEx failed to get size needed");
 		iReturnCode = EDT_ERR_INTERNAL;
@@ -241,17 +246,17 @@ int serviceGetInfoFromServiceHandle(SC_HANDLE schService,  LPCTSTR lpServiceName
 	{
 		//Allocate the buffer
 		dwBufSize = dwBytesNeeded + 0x10; 
-		if(NULL == (pBuf  = (PUCHAR) malloc(dwBufSize)))
+		if((pBuf  = (PUCHAR) malloc(dwBufSize)) == NULL)
 		{
 			LOG_ERROR(L"Memory allocation failed");
 			iReturnCode = EDT_ERR_INTERNAL;
 		}
-		else if( 0 == QueryServiceStatusEx(	schService,				//__in       SC_HANDLE hService,
+		else if( QueryServiceStatusEx(	schService,				//__in       SC_HANDLE hService,
 											SC_STATUS_PROCESS_INFO,	//__in       SC_STATUS_TYPE InfoLevel,
 											pBuf,					//__out_opt  LPBYTE lpBuffer,
 											dwBufSize,				//__in       DWORD cbBufSize,
 											&dwBytesNeeded			//__out      LPDWORD pcbBytesNeeded
-											))
+											) == 0)
 		{
 			switch( GetLastError() ) 
 			{
@@ -269,12 +274,74 @@ int serviceGetInfoFromServiceHandle(SC_HANDLE schService,  LPCTSTR lpServiceName
 		{
 			pStatus = (SERVICE_STATUS_PROCESS*)pBuf; 
 			LOG(L"status = %s\n",serviceGetStatusLabel(pStatus->dwCurrentState));
-			//info->Status=pStatus->dwCurrentState;
-			//info->StatusLabel=serviceGetStatusLabel(info->Status);
 			LOG(L"processId = %d\n",pStatus->dwProcessId);
-			//info->ProcessId=pStatus->dwProcessId;
 		}
 		if(pBuf) free(pBuf);
+	}
+
+	return iReturnCode;
+}
+
+int serviceGetConfigInfoFromServiceHandle(SC_HANDLE schService)
+{
+	int iReturnCode = EDT_OK;
+
+	if(schService==0)
+	{
+		return EDT_ERR_BAD_PARAM;
+	}
+
+	DWORD dwBytesNeeded = 0;
+	DWORD dwBufSize = 0;
+	LPQUERY_SERVICE_CONFIG pBuf = NULL;
+
+	//QueryServiceStatusEx
+	//First call to get the size of the buffer
+	if( QueryServiceConfig(schService,NULL,0,&dwBytesNeeded) != 0 )
+	{
+		LOG_LASTERROR(L"QueryServiceConfig failed to get size needed");
+		iReturnCode = EDT_ERR_INTERNAL;
+	}
+	else
+	{
+		if( ERROR_INSUFFICIENT_BUFFER != GetLastError() || dwBytesNeeded < 1)
+		{
+			LOG_LASTERROR(L"QueryServiceConfig failed to get size needed");
+			iReturnCode = EDT_ERR_INTERNAL;
+		}
+	}
+
+	if(iReturnCode == EDT_OK) 
+	{
+		//Allocate the buffer
+		dwBufSize = dwBytesNeeded + 0x10; 
+		if((pBuf  = (LPQUERY_SERVICE_CONFIG) malloc(dwBufSize)) == NULL)
+		{
+			LOG_ERROR(L"Memory allocation failed");
+			iReturnCode = EDT_ERR_INTERNAL;
+		}
+		else 
+		{
+			if(QueryServiceConfig(schService, pBuf,dwBufSize,&dwBytesNeeded) == 0)
+			{
+				switch( GetLastError() ) 
+				{
+				case NO_ERROR:
+				case ERROR_SERVICE_NOT_ACTIVE:
+				case ERROR_SHUTDOWN_IN_PROGRESS:
+					break;
+				default:
+					LOG_LASTERROR(L"QueryServiceConfig failed");
+					iReturnCode = EDT_ERR_INTERNAL;
+					break;
+				}
+			}
+			else
+			{
+				LOG(L"Log On As %s\n",pBuf->lpServiceStartName);
+			}
+			if(pBuf) free(pBuf);
+		}
 	}
 
 	return iReturnCode;
